2870709c761d585e752ff0e79a2c334778b9715d
KenichiMiyoshi <eagle0344@gmail.com>
2020-06-15 21:49:09 +0900

    認証機能の実装

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyBlogApp.Models;
+
+namespace MyBlogApp.Controllers
+{
+    //誰でもアクセスできるようにする
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        //認証に使うmembershipProviderのインスタンスを保持
+        readonly private CustomMembershipProvider membershipProvider = new CustomMembershipProvider();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
