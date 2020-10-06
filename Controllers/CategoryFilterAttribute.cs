9a0d8407ef4815a541a9506a770e8ee81475de3d
KenichiMiyoshi <eagle0344@gmail.com>
2020-10-06 19:19:16 +0900

    登録済みカデゴリーをdatalistで入力候補に表示

---
+++ Controllers/CategoryFilterAttribute.cs
@@ -0,0 +1,30 @@
+﻿using MyBlogApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlogApp.Controllers
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    //FilterAttributeクラスとIActionFilterインターフェースの実装
+    public class CategoryFilterAttribute : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //コンテキストクラスのインスタンス取得
+            using (var db = new BlogContext())
+            {
+                // Categoryのコレクションを取得
+                var categories = db.Categories.OrderByDescending(item => item.Count).ToList();
+                // ViewBagにcategoriesのデータを保持
+                filterContext.Controller.ViewBag.Categories = categories;
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+        }
+    }
+}
